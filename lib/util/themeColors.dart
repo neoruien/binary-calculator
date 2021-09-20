c81b5b7ccf99a94d1d1ecba3af6edce61483fb3a
neoruien <neoruien123@gmail.com>
2021-09-20 23:35:35 +0800

    Change color scheme

--- lib/util/themeColors.dart
+++ lib/util/themeColors.dart
@@ -2,6 +2,10 @@ const int WHITE = 0xFFFFFFFF;
 const int BLACK = 0xFF000000;
 const int GREY = 0xFFCBCBCB;
 
-const int PRIMARY = 0xFF4FC9CA;
-const int SECONDARY = 0xFF5F7A7A;
-const int TERTIARY = 0xFFF0FDFD;
+const int PRIMARY = 0xFF2490D8;
+const int SECONDARY = 0xFF85B0CE;
+const int TERTIARY = 0xFFEEF8FF;
+
+// const int PRIMARY = 0xFF4FC9CA;
+// const int SECONDARY = 0xFF5F7A7A;
+// const int TERTIARY = 0xFFF0FDFD;
